d3f1d1e4a6d87d6d771c144878a85f604c85dc46
Damble0 <Damble0>
2026-05-22 21:25:05 +0700

    idk apa ini anjir

--- sample-bots-csharp-0.30.0/Crazy/obj/Debug/net6.0/Crazy.AssemblyInfo.cs
+++ sample-bots-csharp-0.30.0/Crazy/obj/Debug/net6.0/Crazy.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Crazy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2823369cff75bf83b9983095c4333958fa440ba7")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("Crazy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Crazy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- sample-bots-csharp-0.30.0/Target/obj/Debug/net6.0/Target.AssemblyInfo.cs
+++ sample-bots-csharp-0.30.0/Target/obj/Debug/net6.0/Target.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Target")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a45f2aef914ab13d729af68b54f4631b24684b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2823369cff75bf83b9983095c4333958fa440ba7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Target")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Target")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
